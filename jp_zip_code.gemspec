7096730859f31113706c1653040ec94ae90628e3
kimromi <kimromi4@gmail.com>
2018-04-24 12:41:38 +0900

    Remove dependency on Hashie

--- jp_zip_code.gemspec
+++ jp_zip_code.gemspec
@@ -21,7 +21,6 @@ Gem::Specification.new do |spec|
   spec.require_paths = ['lib']
 
   spec.add_dependency 'rubyzip'
-  spec.add_dependency 'rash_alt'
   spec.add_development_dependency 'bundler', '~> 1.11'
   spec.add_development_dependency 'rake', '~> 10.0'
   spec.add_development_dependency 'rspec', '~> 3.0'
--- spec/jp_zip_code_spec.rb
+++ spec/jp_zip_code_spec.rb
@@ -3,17 +3,17 @@ require 'spec_helper'
 describe JpZipCode do
   it 'search address data' do
     zip_code_data = JpZipCode.search '8000001'
-    expect(zip_code_data.zip_code).to eq '8000001'
-    expect(zip_code_data.pref_kana).to eq "ﾌｸｵｶｹﾝ"
-    expect(zip_code_data.city_kana).to eq "ｷﾀｷｭｳｼｭｳｼﾓｼﾞｸ"
-    expect(zip_code_data.town_kana).to eq "ｶｻﾞｼ"
-    expect(zip_code_data.pref_kanji).to eq "福岡県"
-    expect(zip_code_data.city_kanji).to eq "北九州市門司区"
-    expect(zip_code_data.town_kanji).to eq "風師"
-    expect(zip_code_data.pref_roman).to eq 'Fukuoka-Ken'
-    expect(zip_code_data.city_roman).to eq 'Moji-Ku Kitakyushu-Shi'
-    expect(zip_code_data.town_roman).to eq 'Kazashi'
-    expect(zip_code_data.pref_code).to eq '40'
+    expect(zip_code_data['zip_code']).to eq '8000001'
+    expect(zip_code_data['pref_kana']).to eq "ﾌｸｵｶｹﾝ"
+    expect(zip_code_data['city_kana']).to eq "ｷﾀｷｭｳｼｭｳｼﾓｼﾞｸ"
+    expect(zip_code_data['town_kana']).to eq "ｶｻﾞｼ"
+    expect(zip_code_data['pref_kanji']).to eq "福岡県"
+    expect(zip_code_data['city_kanji']).to eq "北九州市門司区"
+    expect(zip_code_data['town_kanji']).to eq "風師"
+    expect(zip_code_data['pref_roman']).to eq 'Fukuoka-Ken'
+    expect(zip_code_data['city_roman']).to eq 'Moji-Ku Kitakyushu-Shi'
+    expect(zip_code_data['town_roman']).to eq 'Kazashi'
+    expect(zip_code_data['pref_code']).to eq '40'
 
     [
       nil,
